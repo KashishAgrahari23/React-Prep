8382e02affa31d18b8be70e7fb90181172eb4382
KashishAgrahari23 <kashishagrahari23@gmail.com>
2025-10-28 00:23:02 +0530

    basic prep

--- src/component/FormUseREducer.jsx
+++ src/component/FormUseREducer.jsx
@@ -9,22 +9,47 @@ const initialState ={
 
 function reducer(state , action){
     switch(action.type){
-        case "name" :
+        case "update" :
+            return {
+                ...state , [action.field]:action.value}
+        case "submit":
+            const New = {
+                name:state.name,
+                mail:state.mail,
+                age:state.age
+            }
+            return {
+                ...state , 
+                data:[...state.data , New]
+            }
+        default:
+            return state
+
+            }
 
     }
-}
+
 
 
 const FormUseREducer = () => {
     const [ state , dispatch ] = useReducer(reducer , initialState)
-    const handleSubmit=()=>{
+    const handleChange=(field)=>(e)=>{
+        dispatch({
+            type:"update",
 
+        })
+    }
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+        dispatch({type:"submit"})
     }
   return (
     <div>
-      <form action="Submit" onSubmit={handleClick}>
-            
-
+      <form action="Submit" onSubmit={handleSubmit}>
+            <input type="text" placeholder='enter name' onChange={handleChange("name")}/>
+            <input type="text" placeholder='enter mail' onChange={handleChange("mail")}/>
+            <input type="text" placeholder='enter age' onChange={handleChange("age")}/>
+            <button type="submit">Submit</button>
       </form>
     </div>
   )
